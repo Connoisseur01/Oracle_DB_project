386c13838229ba0627acd09f81b44d414db22857
Connoisseur01 <p.polkowski01@gmail.com>
2022-08-25 13:33:45 +0200

    added new scripts to menu

--- functionality/menu.sql
+++ functionality/menu.sql
@@ -11,31 +11,38 @@ PROMPT [8]  aktualizuj date zakonczenia nauki
 PROMPT [9]  wpisz ocene koncowa
 PROMPT [10] wpisz ocene
 PROMPT [11] zmiana kierunku
+PROMPT ********************** dane osobowe *********************
+PROMPT [12] dodaj osobe
+PROMPT [13] aktualizuj dane osobowe
+PROMPT [14] usun dane osobowe
 PROMPT **********************   raporty   **********************
-PROMPT [12] wypisz oceny per przedmiot_klasa
-PROMPT [13] wypisz oceny ucznia
-PROMPT [14] wyswietl klasy
-PROMPT [15] wyswietl dostepnych nauczycieli
+PROMPT [15] wypisz oceny per przedmiot_klasa
+PROMPT [16] wypisz oceny ucznia
+PROMPT [17] wyswietl klasy
+PROMPT [18] wyswietl dostepnych nauczycieli
 ACCEPT opcja PROMPT "wybierz opcje: "
 
 COLUMN script NEW_VALUE v_script
 
 SELECT CASE '&opcja'
-       WHEN '1' THEN 'obsadz_nauczyciela.sql'
-       WHEN '2' THEN 'przydziel_godziny.sql'
-       WHEN '3' THEN 'usun_przedmiot_nauczyciela.sql'
-       WHEN '4' THEN 'usun_przydzielone_godz.sql'
-       WHEN '5' THEN 'zakoncz_prace.sql'
-       WHEN '6' THEN 'zmien_max_godz.sql'
-       WHEN '7' THEN 'zmiana_wychowawcy.sql'
-       WHEN '8' THEN 'aktual_data_zakonczenia_uczen.sql'
-       WHEN '9' THEN 'wpisanie_oceny_koncowej.sql'
+       WHEN '1'  THEN 'obsadz_nauczyciela.sql'
+       WHEN '2'  THEN 'przydziel_godziny.sql'
+       WHEN '3'  THEN 'usun_przedmiot_nauczyciela.sql'
+       WHEN '4'  THEN 'usun_przydzielone_godz.sql'
+       WHEN '5'  THEN 'zakoncz_prace.sql'
+       WHEN '6'  THEN 'zmien_max_godz.sql'
+       WHEN '7'  THEN 'zmiana_wychowawcy.sql'
+       WHEN '8'  THEN 'aktual_data_zakonczenia_uczen.sql'
+       WHEN '9'  THEN 'wpisanie_oceny_koncowej.sql'
        WHEN '10' THEN 'wpisanie_oceny.sql'
        WHEN '11' THEN 'zmiana_kierunku.sql'
-       WHEN '12' THEN 'wypisz_oceny_per_przedmiot_klasa.sql'
-       WHEN '13' THEN 'wypisz_oceny_ucznia.sql'
-       WHEN '14' THEN 'wyswietl_klasy.sql'
-       WHEN '15' THEN 'pokaz_wolnych_nauczycieli.sql'
+       WHEN '12' THEN 'dodaj_osobe.sql'
+       WHEN '13' THEN 'aktual_dane_sosbowe.sql'
+       WHEN '14' THEN 'usun_dane_osobowe.sql'
+       WHEN '15' THEN 'wypisz_oceny_per_przedmiot_klasa.sql'
+       WHEN '16' THEN 'wypisz_oceny_ucznia.sql'
+       WHEN '17' THEN 'wyswietl_klasy.sql'
+       WHEN '18' THEN 'pokaz_wolnych_nauczycieli.sql'
        ELSE 'menu.sql'
        END AS script
 FROM dual;
