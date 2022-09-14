55b14646587f7b28ccae0f9b5e78562ad01a9ff9
Connoisseur01 <p.polkowski01@gmail.com>
2022-09-14 14:28:41 +0200

    removed polish characters and added apostrophes

--- functionality/wpisanie_oceny_koncowej.sql
+++ functionality/wpisanie_oceny_koncowej.sql
@@ -1,6 +1,6 @@
 ACCEPT pesel PROMPT "podaj pesel ucznia: "
 ACCEPT nazwa_przedmiotu PROMPT "podaj nazwe przedmiotu: "
-ACCEPT ocena PROMPT "podaj wpisywana ocene(oceny calkowite): "
+ACCEPT ocena PROMPT "podaj wpisywana ocene (oceny calkowite): "
 
 EXECUTE pckge_uczniowie.wpisanie_oceny_koncowej(in_pesel => &pesel, in_nazwa_przedmiotu => '&nazwa_przedmiotu', in_ocena => &ocena);
 
--- functionality/wypisz_oceny_per_przedmiot_klasa.sql
+++ functionality/wypisz_oceny_per_przedmiot_klasa.sql
@@ -1,4 +1,4 @@
-ACCEPT klasa PROMPT "podaj klasę: "
+ACCEPT klasa PROMPT "podaj klase: "
 ACCEPT przedmiot PROMPT "podaj przedmiot: "
 
 EXECUTE raporty.wypisz_oceny_per_przedmiot_klasa(in_id_klasy => '&klasa', in_przedmiot => '&przedmiot');
