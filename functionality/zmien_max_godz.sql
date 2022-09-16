90430e86fcc35d374f69a123ec02353209fd742c
Connoisseur01 <p.polkowski01@gmail.com>
2022-09-16 13:59:20 +0200

    added package name to procedure call

--- functionality/zmien_max_godz.sql
+++ functionality/zmien_max_godz.sql
@@ -1,6 +1,6 @@
 ACCEPT id_nauczyciela PROMPT "podaj id_nauczyciela: "
 ACCEPT max_godz PROMPT "podaj nowa maxymalna liczbe godzin w tygodniu:"
 
-EXECUTE zmien_max_godz (in_nauczyciel => &id_nauczyciela, in_max_godz => &max_godz);
+EXECUTE pckge_nauczyciele.zmien_max_godz (in_nauczyciel => &id_nauczyciela, in_max_godz => &max_godz);
 
 @@menu.sql
